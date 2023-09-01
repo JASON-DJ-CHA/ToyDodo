a345e1fb67ecfc43f409de56795830770aeb0077
jason <jason.c9340@gmail.com>
2023-09-01 18:49:47 +0900

    리얼타임db는 사용할때 US서버를 사용하자..

--- app/src/main/java/com/example/toytodoapplication/view/main/HomeFragment.kt
+++ app/src/main/java/com/example/toytodoapplication/view/main/HomeFragment.kt
@@ -19,6 +19,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class HomeFragment : Fragment(), AddTodoFragment.DialogNextBtnClickListener,
